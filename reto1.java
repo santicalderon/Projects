8e083027ee2eef07dcf57bc5d682098300b2eede
santicalderon <calderonsantiago98@gmail.com>
2021-07-22 20:11:49 -0500

    ciclo 2 reto 1 finalizado

---
+++ reto1.java
@@ -0,0 +1,14 @@
+
+public class reto1 {
+    public static String compararFranquicia(int pTiempo, int pCapital, double pInteres){
+        double interesSimple = pCapital*pInteres*pTiempo;
+        double interesCompuesto = pCapital*(Math.pow(1+pInteres, pTiempo)-1); 
+        double diferencia = interesCompuesto - interesSimple;
+        String texto = ("La diferencia en el total de intereses generados para el proyecto, si escogemos entre evaluarlo a una tasa de interés Compuesto y evaluarlo a una tasa de interés Simple, asciende a la cifra de: $" + diferencia);        
+        String faltanDatos = ("Faltan datos para calcular la diferencia en el total de intereses generados para el proyecto.");
+        if (diferencia == 0) {
+            return faltanDatos;
+        }
+        return texto;
+    }
+}
